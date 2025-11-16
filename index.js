ae6f2d5d12d698713544c3cf5838d2d285760378
usmanbankagmail <usmanbanka@gmail.com>
2025-11-16 14:35:12 +0500

    chore: Practicing how to connect to Mongo DB through Node JS and using Environment Variables

---
+++ index.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function connectDB() {
+
+    try {
+
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Congratulations! MongoDB Connected Successfully!");
+        process.exit(0);
+    }
+
+    catch(error) {
+        console.log("Sorry, MongoDB couldn't connect due to the following error:");
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+connectDB();
